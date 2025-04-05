31c04813dec630b9a615731c0d56e10a193c5442
Ayush Datta <135964513+Ayushdatta12@users.noreply.github.com>
2025-04-05 19:18:20 +0530

    Add files via upload

---
+++ web AI/website creator.py
@@ -0,0 +1,20 @@
+import google.generativeai as genai
+import os
+import webbrowser
+import random
+#from dotenv import load_dotenv
+gemkey='AIzaSyCUVaJty3Wo9mI0EpV5pOZD2joqNV1whyQ'
+def create_html_file(filename, content):
+    with open(filename, 'w') as f:
+        f.write(content)
+genai.configure(api_key=gemkey)
+model = genai.GenerativeModel("gemini-2.0-flash-exp")
+descr=input("Enter description for your site \n :")
+filename=input("enter a file name to save the site as \n :")
+prompt=f"Generate an HTML code based on the descsription given below use CSS and Java script if required and put them under the <style> and <script> section of the html file respectively description=```{descr}``` don't put it in a code block or give any explanation just return the html code in plain text format, if the user provides any links for css styles use that instead, start with <html>"
+print("Generating Site")
+response = model.generate_content(prompt)
+print(response)
+create_html_file((f"{filename}.html"),response.text)
+webbrowser.open_new_tab(f'{filename}.html') 
+input("Press Enter to exit...")
